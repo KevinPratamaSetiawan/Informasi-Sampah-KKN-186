4785882fcf21309997e41860723eea2b59ec7b54
KevinPratamaSetiawan <127008007+KevinPratamaSetiawan@users.noreply.github.com>
2024-01-15 23:35:42 +0700

    Loading Screen

--- script.js
+++ script.js
@@ -8,3 +8,13 @@ window.onscroll = function() {
   }
   prevScrollpos = currentScrollPos;
 }
+
+document.onreadystatechange = function() {
+  if (document.readyState !== "complete") {
+      document.querySelector("body").style.visibility = "hidden";
+      document.querySelector("#loading-screen").style.visibility = "visible";
+  } else {
+      document.querySelector("#loading-screen").style.display = "none";
+      document.querySelector("body").style.visibility = "visible";
+  }
+};
